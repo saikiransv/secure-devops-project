04b7ec26a92f183f5a6227ca48ad8860d52619b3
saikiransv <saikiransv2021@gmail.com>
2026-03-26 20:25:37 +0530

    test vulnerable 111

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
+# app/app.py
+
 from flask import Flask, request
 import sqlite3
 import os
 
 app = Flask(__name__)
 
-# ❌ Hardcoded secret (WILL trigger Secret Scan)
-app.config['SECRET_KEY'] = "super_secret_key_123456"
+# ❌ Hardcoded secret (secret-scan FAIL)
+app.config['SECRET_KEY'] = "ghp_abcd1234verylongsecrettokenxyz"
 
 def get_db():
     return sqlite3.connect("users.db")
@@ -22,7 +24,7 @@ def login():
     conn = get_db()
     cursor = conn.cursor()
 
-    # ❌ SQL Injection (SAST will detect)
+    # ❌ SQL Injection (SAST FAIL)
     query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
     cursor.execute(query)
 
@@ -35,11 +37,11 @@ def login():
 def cmd():
     user_input = request.args.get("cmd")
 
-    # ❌ Command Injection (SAST will detect)
+    # ❌ Command Injection (SAST FAIL)
     os.system(user_input)
 
     return "Command executed"
 
 if __name__ == "__main__":
-    # ❌ Debug mode ON (SAST will detect)
-    app.run(debug=True)
+    # ❌ Debug ON (SAST FAIL)
+    app.run(host="0.0.0.0", port=5000, debug=True)
