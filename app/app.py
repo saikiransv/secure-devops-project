3b5b80fed12711dd36f6c4a0f6e9c10d7c823ec4
saikiransv <saikiransv2021@gmail.com>
2026-04-02 22:25:40 +0530

    Test-167

--- app/app.py
+++ app/app.py
@@ -6,37 +6,15 @@ import os
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 # ❌ REALISTIC SECRET (WILL BE DETECTED)
 app.config['SECRET_KEY'] = "ghp_abcd1234abcd1234abcd1234abcd1234abcd"
 
-=======
-# ✅ Secure: No hardcoded fallback
-# secret = os.getenv("SECRET_KEY")
-# if not secret:
-#     raise RuntimeError("SECRET_KEY environment variable not set")
-
-# app.config['SECRET_KEY'] = secret
-
-# Connect to DB
->>>>>>> 88a8d36441f1b2ec6c47c8514ffb6b6aad657c20
 def get_db():
     return sqlite3.connect("users.db")
 
 @app.route("/")
 def home():
-<<<<<<< HEAD
     return "Vulnerable App Running"
-=======
-    return """
-    <h1>Secure DevOps Project</h1>
-
-    <a href="/dashboard" target="_blank">
-        <button>Open Dashboard</button>
-    </a>
-    """
-
->>>>>>> 88a8d36441f1b2ec6c47c8514ffb6b6aad657c20
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -50,35 +28,8 @@ def login():
     query = "SELECT * FROM users WHERE username='" + username + "' AND password='" + password + "'"
     cursor.execute(query)
 
-<<<<<<< HEAD
     if cursor.fetchone():
         return "Login Success"
-=======
-    user = cursor.fetchone()
-    conn.close()
-
-
-@app.route("/dashboard")
-def dashboard():
-    return """
-    <h1>Security Dashboard</h1>
-    <button onclick="loadData()">Load Reports</button>
-
-    <pre id="output"></pre>
-
-    <script>
-    async function loadData() {
-        const data = await fetch('/api/sast').then(r => r.json());
-        document.getElementById('output').innerText =
-            JSON.stringify(data, null, 2);
-    }
-    </script>
-    """
-
-
-    if user:
-        return "Login successful"
->>>>>>> 88a8d36441f1b2ec6c47c8514ffb6b6aad657c20
     else:
         return "Login Failed"
 
